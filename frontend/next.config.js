25b778ec17a0132eaf3b349c0b0dfd744be94f91
ash-codiology <ashfachohan7@gmail.com>
2026-01-13 23:17:05 +0500

    Deploy static Next.js build to gh-pages

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  swcMinify: true,
-  trailingSlash: true,
-}
-
-module.exports = nextConfig
